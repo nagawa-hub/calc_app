3e7a03f60278c18f64e957ca3ef1aa5a26a6e196
nagawa-hub <nakagawayu2008@gmail.com>
2021-02-02 13:18:42 +0900

    Imageコンポーネントを作成

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import Link from "next/link";
 import Layout from "../components/Layout";
+import Image from "../components/Image";
 
 export default () =>(
   <Layout header="Next" title="Top page.">
     <p>Welcome</p>
+    <Image fname="cat.jpg" size="250" />
     <hr/>
     <Link href="/other">
       <button>Go to Other page &gt;&gt;</button>
