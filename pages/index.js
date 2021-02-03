2b451ad77df498f07007f50c281ee41a9bfeec5d
nagawa-hub <nakagawayu2008@gmail.com>
2021-02-03 12:17:22 +0900

    reduxを用いたcounterに修正

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,10 @@
 import Link from "next/link";
 import Layout from "../components/Layout";
-import Image from "../components/Image";
+import Counter from "../components/Counter";
 
 export default () =>(
   <Layout header="Next" title="Top page.">
-    <p>Welcome</p>
-    <Image fname="cat.jpg" size="250" />
+    <Counter />
     <hr/>
     <Link href="/other">
       <button>Go to Other page &gt;&gt;</button>
